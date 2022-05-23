a66f9ab38330ed0c2df8b744cf7ec5a2328c3207
Nick Molina <Nicholasmolina15@gmail.com>
2022-05-23 11:17:57 -0500

    connected mongo

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require ("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
+
+const DATABASE_URL = 'mongodb+srv://admin:abc1234@cluster0.nku8p.mongodb.net/tweeter?retryWrites=true&w=majority'
+const db = mongoose.connection;
+mongoose.connect(DATABASE_URL);
 
 app.listen(PORT,()=>{
  console.log(`express is listening on port`, PORT);
